f21eb2a6547bc7a5899e7239d374af0438795827
Kiki Huang <kikihuang@teamt5.org>
2022-04-29 17:56:30 +0800

    test: add GetFileSize test function

--- test/t_main.cc
+++ test/t_main.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "gtest/gtest.h"
 #include "src/callback.h"
+#include "src/query_file.h"
 
 TEST(Callback, LogFile) {
   int count = 0;
@@ -10,5 +11,19 @@ TEST(Callback, LogFile) {
   ASSERT_EQ(ret, true);
 }
 
+TEST(Callback, GetFileSize) {
+  unsigned long long size = 0;
+  unsigned long long* sizePtr = &size;
+  auto lambda = [](const std::wstring& wstrDir, HANDLE hFind,
+                   WIN32_FIND_DATA findData, void* sizePtr) -> bool {
+    unsigned long long* sizeULLPtr = (unsigned long long*)sizePtr;
+    unsigned long long dwSize =
+        (findData.nFileSizeHigh * (MAXDWORD + 1)) + findData.nFileSizeLow;
+    *sizeULLPtr += dwSize;
+    return true;
+  };
+
+  auto ret = QueryFile(L"C:\\Users\\kikihuang\\Documents", sizePtr, lambda);
+  std::cout << "All Directory Size: " << *sizePtr << std::endl;
   ASSERT_EQ(ret, true);
 }
