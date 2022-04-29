31763ebe0a364c7b80d345a7e6ff3487036c261f
Kiki Huang <kikihuang@teamt5.org>
2022-04-29 13:23:21 +0800

    feat: add LogFile function

--- main.cc
+++ main.cc
@@ -1,10 +1,15 @@
 #include <windows.h>
 #include <functional>
 #include <iostream>
+#include <string>
 
-bool QueryFile(std::wstring wstrDir,
-               std::function<bool(std::wstring, int)> file_callback) {
-  int count = 0;
+bool LogFile(const std::wstring& wstrDir, int count) {
+  std::wcout << wstrDir << std::endl;
+  return true;
+}
+
+bool QueryFile(const std::wstring& wstrDir,
+               bool (*file_callback)(const std::wstring&, int)) {
   if (wstrDir.empty()) {
     return false;
   }
@@ -24,12 +29,14 @@ bool QueryFile(std::wstring wstrDir,
     wstrFileName.assign(wstrDir);
     wstrFileName.append(L"\\");
     wstrFileName.append(findData.cFileName);
-
-    auto ret = file_callback(wstrFileName.c_str(), 15);
-    if (ret == false) continue;
-
+    int count = 0;
     if (findData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {  // 是否是目錄
-      QueryFile(wstrFileName.c_str(), file_callback);
+      wstrFileName.append(L"\\");
+      QueryFile(wstrFileName, file_callback);
+    } else {
+      count++;
+      auto ret = file_callback(wstrFileName, count);
+      if (ret == false) break;
     }
   } while (FindNextFile(hFind, &findData) != 0);
 
@@ -37,32 +44,12 @@ bool QueryFile(std::wstring wstrDir,
   return true;
 }
 
-// bool queryfile(function<bool(std::string, std::string, int)> log_func) {
-//   //模擬 file query.
-//   for (auto i = 0; i < 20; i++) {
-//     std::string path = "C:\\123123123.exe";
-//     std::string path2 = "D:\\aaaaa.exe";
-//     int ret = log_func(path, path2, i);
-//     if (ret == false)  //可能有些地方需要跳開
-//       break;
-//   }
-
-//   return true;
-// }
-
-// //callback + lambda()
-//   queryfile([](std::string path , std::string path2 , int count){
-
-//     //一些處理
-//     std::cout << "path:" << path << std::endl;
-//     std::cout << "path2:" << path2 << std::endl;
-//     std::cout << "count:" << count << std::endl;
-//     if (count > 8) // 跳掉判斷...
-//     {
-//       return false;
-//     }
-
-//     return true;
-//   });
-
-int main() { return 0; }
+int main() {
+  setlocale(LC_ALL, setlocale(LC_CTYPE, ""));
+  try {
+    QueryFile(L"C:\\Users\\kikihuang\\Documents", LogFile);
+  } catch (const std::exception& e) {
+    std::cerr << e.what();
+  }
+  return 0;
+}
