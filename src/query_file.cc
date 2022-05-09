159954b080ddb87b67d5bfaa2e30e829bf750a4e
Kiki Huang <kikihuang@teamt5.org>
2022-05-09 15:14:26 +0800

    style: just formatting

--- src/query_file.cc
+++ src/query_file.cc
@@ -4,28 +4,18 @@
 
                std::function<bool(const std::wstring&, WIN32_FIND_DATA,
                                   void*)>  // 不要用 void*，用 struct
-  if (wstrDir.empty()) {
-    return false;
-  }
-  HANDLE hFind;
-  WIN32_FIND_DATA findData;
+  if (wstrDir.empty()) return false;
+
   std::wstring wstrTempDir = wstrDir + (L"\\*");
-  hFind = FindFirstFile(wstrTempDir.c_str(), &findData);
-  if (hFind == INVALID_HANDLE_VALUE) {
-    return false;
-  }
+  WIN32_FIND_DATA findData;
+  HANDLE hFind = FindFirstFile(wstrTempDir.c_str(), &findData);
+  if (hFind == INVALID_HANDLE_VALUE) return false;
   do {
     if (wcscmp(findData.cFileName, L".") == 0 ||
         wcscmp(findData.cFileName, L"..") == 0)
       continue;
 
-    std::wstring wstrFileName;
-    wstrFileName.assign(wstrDir);
-    wstrFileName.append(L"\\");
-    wstrFileName.append(findData.cFileName);
-
-    int file_count = 0;
-    const int FILE_MAX_LIMIT = 500000;
+    std::wstring wstrFileName = wstrDir + (L"\\") + findData.cFileName;
 
     if (file_count > FILE_MAX_LIMIT) break;
     if (findData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {  // 是否是目錄
