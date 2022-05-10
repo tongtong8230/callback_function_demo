bf274604d9605c29b81c543c3fce1845b9f23dc9
Kiki Huang <kikihuang@teamt5.org>
2022-05-10 17:15:35 +0800

    feat: add file_max_limit in CountFile

--- src/callback.cc
+++ src/callback.cc
@@ -2,8 +2,8 @@
 
 bool CountFile(const std::wstring& wstrDir, const WIN32_FIND_DATA& ffd,
                DirectoryData* dir_ptr) {
-  const unsigned long long FILE_MAX_LIMIT = 500000;
-  if (dir_ptr->file_count > FILE_MAX_LIMIT) return true; // 要 return true or false?
+  const unsigned long long FILE_MAX_LIMIT = 100000;
+  if (dir_ptr->file_count >= FILE_MAX_LIMIT) return false;
   dir_ptr->file_count++;
   return true;
 }
