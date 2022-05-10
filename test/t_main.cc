479c5b1712bf659132a3412047353aaaec1d38b1
Kiki Huang <kikihuang@teamt5.org>
2022-05-10 17:16:19 +0800

    feat: add file_max_limit in GetFileSize

--- test/t_main.cc
+++ test/t_main.cc
@@ -16,6 +16,9 @@ TEST(Callback, GetFileSize) {
   DirectoryData* dir_ptr = &dir_data;
   auto lambda = [](const std::wstring& wstrDir, const WIN32_FIND_DATA& ffd,
                    DirectoryData* dir_ptr) -> bool {
+    const unsigned long long FILE_MAX_LIMIT = 500000;
+    if (dir_ptr->file_count > FILE_MAX_LIMIT) return false;
+
     unsigned long long dwSize =
         (ffd.nFileSizeHigh * (MAXDWORD + 1)) + ffd.nFileSizeLow;
     dir_ptr->file_total_size += dwSize;
